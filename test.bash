8d0c938b387ea3a499a4be38bcfb46233b2ec0ab
Reoto Koya <s24c1053jd@s.chibakoudai.jp>
2025-12-25 15:06:37 +0900

    test.bashの中身をrel_erコマンドni書き換え

--- test.bash
+++ test.bash
@@ -9,88 +9,42 @@ ng () {
 
 res=0
 
-out=$(echo 100 | ./as_f)
-[ "${out}" = "2 2 5 5" ] || ng "$LINENO"
+out=$(echo "10 10.5" | ./rel_er)
+[ "${out}" = "5.00%" ] || ng "$LINENO"
 
-out=$(echo 45 | ./as_f)
-[ "${out}" = "3 3 5" ] || ng "$LINENO"
+out=$(echo -e "10 10.5\n9 9.5" | ./rel_er)
+expected=$'5.00%\n5.56%'
+[ "${out}" = "$expected"  ] || ng "$LINENO"
 
-out=$(echo 97 | ./as_f)
-[ "${out}" = "97" ] || ng "$LINENO"
-
-out=$(echo abc | ./as_f)
+out=$(echo "0 1" | ./rel_er )
+[ "$?" = 1 ] || ng "$LINEO"
 [ "${out}" = "" ] || ng "$LINENO"
 
-out=$(echo "10 7" | ./as_f)
-expected=$'2 5\n7'
-[ "${out}" = "$expected" ] || ng "$LINENO"
-
-out=$(echo -10 | ./as_f)
+out=$(echo -e  "10 10.5 11\n2 3" | ./rel_er )
+[ "$?" = 2 ] || ng "$LINEO"
 [ "${out}" = "" ] || ng "$LINENO"
 
-out=$(echo 0.1 | ./as_f)
+out=$(echo "" | ./rel_er )
+[ "$?" = 2 ] || ng "$LINEO"
+[ "${out}" = "" ] || ng "$LINENO"
+
+out=$(echo "" | ./rel_er )
+[ "$?" = 2 ] || ng "$LINEO"
+[ "${out}" = "" ] || ng "$LINENO"
+
+out=$(echo "10 10.5\n9 9.5" | ./rel_er)
+[ "$?" = 3 ] || ng "$LINEO"
+[ "${out}" = ""  ] || ng "$LINENO"
+
+out=$(echo "q" | ./rel_er )
+[ "$?" = 3 ] || ng "$LINEO"
+[ "${out}" = "" ] || ng "$LINENO"
+
+out=$(echo "q 2" | ./rel_er )
+[ "$?" = 3 ] || ng "$LINEO"
 [ "${out}" = "" ] || ng "$LINENO"
 
 
 [ "${res}" = 0 ] && echo OK
 
 exit $res
-
-out=$(echo 100 | ./as_f)
- ++ echo 100
- ++ ./as_f
- + out=2 2 5 5
-[ "${out}" = "2 2 5 5" ] || ng "$LINENO"
-+ '[' 2 2 5 5 = 2 2 5 5 ']'
-
-out=$(echo 45 | ./as_f)
-++ echo 45
-++ ./as_f
-+ out=3 3 5
-[ "${out}" = "3 3 5" ] || ng "$LINENO"
-+ '[' 3 3 5 = 3 3 5']'
-
-out=$(echo 97 | ./as_f)
-++ echo 97
-++ ./as_f
-+ out=97
-[ "${out}" = "97" ] || ng "$LINENO"
-+ '[' 97 = 97 ']'
-
-out=$(echo abc | ./as_f)
-++ echo abc
-++ ./as_f
-+ out= 
-[ "${out}" = "" ] || ng "$LINENO"
-+ '[' '' = '' ']'
-
-out=$(echo "10 7" | ./as_f)
-++ echo 10 7
-++ ./as_f
-+ out=2 5
-7
-[ "${out}" = "$expected" ] || ng "$LINENO"
-+ '[' '2 5
-7' = '2 5
-7' ']'
-
-out=$(echo -10 | ./as_f)
-++ echo -10
-++ ./as_f
-+ out= 
-[ "${out}" = "" ] || ng "$LINENO"
-+ '[' '' = '' ']'
-
-out=$(echo 0.1 | ./as_f)
-++ echo 0.1
-++ ./as_f
-+ out= 
-[ "${out}" = "" ] || ng "$LINENO"
-+ '[' '' = '' ']'
-
-[ "${res}" = 0 ] && echo OK
-+ '[' 0 = 0 ']'
-+ echo OK
-OK
-exit $res
-+ exit 0
